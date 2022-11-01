8d9e6e7a54c084ec656f77e79d4fa7a836ce5f04
PolRivarola <rivarolapol@gmail.com>
2022-10-31 21:23:07 -0300

    pretty

--- src/grafo/Grafo.java
+++ src/grafo/Grafo.java
@@ -7,113 +7,107 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Queue;
-import java.util.Scanner;
 
-public class Grafo   {
-	
-	private Map<String, Nodo>NodoMap = new HashMap<String,Nodo>();
-	Scanner s = new Scanner(System.in);
+public class Grafo {
 
-	
+	private Map<String, Nodo> NodoMap = new HashMap<String, Nodo>();
+
 	public Nodo getNodo(String NodoName) {
 		Nodo n = NodoMap.get(NodoName);
 		return n;
 	}
-	
-	public void addNodo(String NodoName,String NodoName2, int cost) {
+
+	public void addNodo(String NodoName, String NodoName2, int cost) {
 		Nodo n = NodoMap.get(NodoName);
 		Nodo m = NodoMap.get(NodoName2);
-		if(n == null) {
+		if (n == null) {
 			n = new Nodo(NodoName);
-			NodoMap.put(NodoName,n);
-			System.out.println("Nodo: "+ NodoName +" no encontrado... nuevo nodo creado");
+			NodoMap.put(NodoName, n);
+			System.out.println("Nodo: " + NodoName + " no encontrado... nuevo nodo creado");
 		}
-		if(m == null) {
+		if (m == null) {
 			n = new Nodo(NodoName2);
-			NodoMap.put(NodoName2,n);
-			System.out.println("Nodo: "+ NodoName2 +" no encontrado... nuevo nodo creado");
-		}	
-		agregarArista(NodoName, NodoName2, cost);
-		
-			
+			NodoMap.put(NodoName2, n);
+			System.out.println("Nodo: " + NodoName2 + " no encontrado... nuevo nodo creado");
+		}
+		addArista(NodoName, NodoName2, cost);
+
 	}
 
-	
-	
 	public void searchGraphs() {
 		for (Nodo value1 : NodoMap.values()) {
 			for (Nodo value2 : NodoMap.values()) {
-			    printAllPaths(value1, value2);
+				clearAll();
+				if (value2 != value1) {
+					printAllPaths(value1, value2);
+				}
+
 			}
-			clearAll();
 		}
 	}
-	
-	
+
 	public void seeGraph() {
-		if(NodoMap != null) {
-			for(Map.Entry<String, Nodo> entry : NodoMap.entrySet()) {
+		if (NodoMap != null) {
+			for (Map.Entry<String, Nodo> entry : NodoMap.entrySet()) {
 				System.out.println("Nodo: " + entry.getKey());
 				for (Arista e : entry.getValue().adjacente) {
-					System.out.println("  -Apunta a: "+ e.destino.name + "(" + e.costo +")");
+					System.out.println("  -Apunta a: " + e.destino.name + "(" + e.costo + ")");
 				}
 			}
 		} else {
-			System.out.println("No existen vertices ni aristas en este grafo");
+			System.out.println("No existen nodos ni arcos en este grafo");
 		}
 	}
-	
-	
+
 	public void dijkstra(String startName, String destino) {
 		clearAll();
 		Nodo start = NodoMap.get(startName);
 		Nodo end = NodoMap.get(destino);
 
 		start.dist = 0;
-		
+
 		Queue<Nodo> pq = new LinkedList<Nodo>();
-		
+
 		pq.add(start);
-		while(!pq.isEmpty() ) {
+		while (!pq.isEmpty()) {
 			Nodo v = pq.remove();
-			for(Arista e : v.adjacente) {
-			Nodo w = e.destino;
-			int cVW = e.costo;
+			for (Arista e : v.adjacente) {
+				Nodo w = e.destino;
+				int cVW = e.costo;
 
-		
-			if( w.dist == -1) {
+				if (w.dist == -1) {
 
-				w.dist = v.dist + cVW;
-				w.prev = v;
-				pq.add(w);}
-			else if (w.dist > v.dist + cVW) {
-				w.dist = v.dist + cVW;
-				w.prev = v;
-				pq.add(w);
+					w.dist = v.dist + cVW;
+					w.prev = v;
+					pq.add(w);
+				} else if (w.dist > v.dist + cVW) {
+					w.dist = v.dist + cVW;
+					w.prev = v;
+					pq.add(w);
 				}
-			
-			
+
 			}
 		}
 		if (end.dist != -1) {
-			System.out.println("La suma de los pesos del camino mas corto entre " + startName+ " y " + end.name + " es: " + end.dist);
-		}else System.out.println("No hay camino entre estos nodos");
+			System.out.println("La suma de los pesos del camino mas corto entre " + startName + " y " + end.name
+					+ " es: " + end.dist);
+		} else
+			System.out.println("No hay camino entre estos nodos");
 	}
-	
+
 	public void BFS(String startName, String destino) {
 		clearAll();
 		Nodo start = NodoMap.get(startName);
 		Nodo end = NodoMap.get(destino);
 
-		
 		Queue<Nodo> q = new LinkedList<Nodo>();
 		q.add(start);
 		start.dist = 0;
-		
+
 		while (!q.isEmpty()) {
 			Nodo v = q.remove();
-			
-			for( Arista e : v.adjacente) {
+
+			for (Arista e : v.adjacente) {
 				Nodo w = e.destino;
 				if (w.dist == -1) {
 					w.dist = v.dist + 1;
@@ -123,106 +117,53 @@ public class Grafo   {
 			}
 		}
 		if (end.dist != -1) {
-			System.out.println("La distancia entre " + startName+ " y " + end.name + " es: " + end.dist);
-		}else System.out.println("No hay camino entre estos nodos");
+			System.out.println("La distancia entre " + startName + " y " + end.name + " es: " + end.dist);
+		} else
+			System.out.println("No hay camino entre estos nodos");
 	}
-	
+
 	private void clearAll() {
 		for (Entry<String, Nodo> e : NodoMap.entrySet()) {
 			e.getValue().dist = -1;
 			e.getValue().setVisited(false);
-			}  
+		}
 	}
-	
-	// Prints all paths from
-    // 's' to 'd'
-    public void printAllPaths(Nodo s, Nodo d)
-    {
-        ArrayList<String> pathList = new ArrayList<>();
- 
-        // add source to path[]
-        pathList.add(s.name);
- 
-        // Call recursive utility
-        printAllPathsUtil(s, d, pathList);
-    }
- 
-    // A recursive function to print
-    // all paths from 'u' to 'd'.
-    // isVisited[] keeps track of
-    // vertices in current path.
-    // localPathList<> stores actual
-    // vertices in the current path
-    private void printAllPathsUtil(Nodo u, Nodo d,
-                                   
-                                   List<String> localPathList)
-    {
- 
-        if (u.equals(d)) {
-            System.out.println(localPathList);
-            // if match found then no need to traverse more till depth
-            return;
-        }
- 
-        // Mark the current node
-        u.setVisited(true); 
- 
-        // Recur for all the vertices
-        // adjacent to current vertex
-        for (Arista i : u.adjacente) {
-            if (!i.destino.isVisited()) {
-                // store current node
-                // in path[]
-                localPathList.add(i.destino.name);
-                printAllPathsUtil(i.destino, d, localPathList);
- 
-                // remove current node
-                // in path[]
-                localPathList.remove(i.destino.name);
-            }
-        }
- 
-        // Mark the current node
-        u.setVisited(true);
-    }
-	
-	
-	public boolean hasCycle(Nodo sourceNodo) {
-		
-	    sourceNodo.setBeingVisited(true);
-	    sourceNodo.filaNoC.add(sourceNodo.name);
 
-	    for (Arista neighbor : sourceNodo.adjacente) {
-	        if (neighbor.destino.isBeingVisited()) {
-	        	
-	        	sourceNodo.filaNoC.forEach(System.out::print);;
-	        	System.out.print(" ");
-	        	
-	        	sourceNodo.filaNoC.clear();;
-	        
+	public void printAllPaths(Nodo s, Nodo d) {
+		ArrayList<String> pathList = new ArrayList<>();
 
-	            return true;
-	        } else if (!neighbor.destino.isVisited() && hasCycle(neighbor.destino)) {
-	        
-	        	sourceNodo.filaNoC.forEach(System.out::print);;
-	        	System.out.print(" ");
-	        	sourceNodo.filaNoC.clear();;
-	        	return true;
-	        }
-	    }
+		pathList.add(s.name);
 
-	    sourceNodo.setBeingVisited(false);
-	    sourceNodo.setVisited(true);
-	    System.out.println();
-	    return false;
+		printCaminosUtil(s, d, pathList);
 	}
-	
-	public void agregarArista(String nomOri, String nomDest, int cost) {
+
+	private void printCaminosUtil(Nodo u, Nodo d, List<String> localPathList) {
+
+		if (u.equals(d)) {
+			System.out.println(localPathList);
+			return;
+		}
+
+		u.setVisited(true);
+
+		for (Arista i : u.adjacente) {
+			if (!i.destino.isVisited()) {
+
+				localPathList.add(i.destino.name);
+				printCaminosUtil(i.destino, d, localPathList);
+
+				localPathList.remove(i.destino.name);
+			}
+		}
+
+		u.setVisited(true);
+	}
+
+
+	public void addArista(String nomOri, String nomDest, int cost) {
 		Nodo v = getNodo(nomOri);
 		Nodo w = getNodo(nomDest);
-		v.adjacente.add(new Arista(w,cost));
-		
-	
-		
+		v.adjacente.add(new Arista(w, cost));
+
 	}
 }
--- src/grafo/Nodo.java
+++ src/grafo/Nodo.java
@@ -1,7 +1,6 @@
 package grafo;
 
 import java.util.List;
-import java.util.Queue;
 import java.util.LinkedList;
 
 public class Nodo implements Comparable<Nodo> {
@@ -10,33 +9,25 @@ public class Nodo implements Comparable<Nodo> {
 	public List<String> filaNoC;
 	public int dist;
 	public Nodo prev;
-	private boolean beingVisited;
 	private boolean visited;
-	
-	public Nodo(String nm)   {
+
+	public Nodo(String nm) {
 		name = nm;
 		adjacente = new LinkedList<Arista>();
 		filaNoC = new LinkedList<String>();
 		reset();
-		
+
 	}
-	
+
 	public int compareTo(Nodo other) {
-	    return Integer.compare(this.dist, other.dist);
+		return Integer.compare(this.dist, other.dist);
 	}
-	
+
 	public void reset() {
 		dist = -1;
 	}
 
-	public boolean isBeingVisited() {
-		return beingVisited;
-	}
-
-	public void setBeingVisited(boolean beingVisited) {
-		this.beingVisited = beingVisited;
-	}
-
+	
 	public boolean isVisited() {
 		return visited;
 	}
